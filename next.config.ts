954f4c4b3c2f37240622fe183c0c9846c87dbd42
eze <cadiz.edward@gmail.com>
2026-06-12 09:46:31 -0400

    preperations to host oh github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/my-project',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
