b8c3b3ad2916820dbb44491e8a9011f1b180f722
eze <cadiz.edward@gmail.com>
2026-06-12 10:19:10 -0400

    Revert "preperations to host oh github pages"
    This reverts commit 954f4c4b3c2f37240622fe183c0c9846c87dbd42.

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  basePath: '/my-project',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
